a20930ac44783a06f98170d316bf001bcad96a96
Осетров Павел <p.osetrov@ctm.ru>
2022-07-04 08:34:50 +0300

    bug fixes

--- lib/generators/sblogistica/install/templates/sblogistica.rb
+++ lib/generators/sblogistica/install/templates/sblogistica.rb
@@ -1,14 +1,14 @@
-require 'dashamail'
+require 'sblogistica'
 
-Dashamail.setup do |config|
-  if File.exist?('config/dashamail.yml')
-    processed = YAML.load_file('config/dashamail.yml')[Rails.env]
+Sblogistica.setup do |config|
+  if File.exist?('config/sblogistica.yml')
+    processed = YAML.load_file('config/sblogistica.yml')[Rails.env]
 
     processed.each do |k, v|
       config::register k.underscore.to_sym, v
     end
 
-    config::Request.api_key = Dashamail::api_key || ENV['DASHAMAIL_API_KEY']
+    config::Request.api_key = Sblogistica::api_key || ENV['SBLOGISTICA_API_KEY']
     config::Request.timeout = 15
     config::Request.open_timeout = 15
     config::Request.symbolize_keys = true
